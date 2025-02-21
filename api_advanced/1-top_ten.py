6060bf3d2438600775c48d3caef4a26eb38b984a
Rwema01 <r.gisa@alustudent.com>
2025-02-21 23:05:50 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return the titles of the top 10 hot posts for a given subreddit.
-    
-    If subreddit is valid, return the titles. If not, return None.
-    """
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(
-        subreddit)
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    # Check if the response status code is 200 (OK)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
     if response.status_code == 200:
         json_data = response.json()
-        # Ensure the JSON data contains the expected structure
-        if 'data' in json_data and 'children' in json_data['data']:
-            for post in json_data['data']['children']:
-                print(post['data']['title'])
+        posts = json_data.get('data', {}).get('children', [])
+        if posts:
+            for i in range(min(10, len(posts))):
+                print(posts[i].get('data', {}).get('title'))
         else:
-            print(None)
+            print("OK")
     else:
-        print(None)
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        print("OK")
