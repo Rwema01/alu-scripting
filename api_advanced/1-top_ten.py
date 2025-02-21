0fab48475bbd0f7c6ecc2bbe996c92e20fec3dd5
Rwema01 <r.gisa@alustudent.com>
2025-02-21 21:17:19 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,19 +7,20 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed for a given subreddit."""
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url, subreddit=subreddit)
+    base_url = "https://www.reddit.com"
+    api_uri = f"{base_url}/r/{subreddit}/hot.json"
 
     # Set a User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
+    user_agent = {"User-Agent": "Python/requests"}
 
     # Set the Query Strings to Request
-    payload = {'limit': 10}
+    payload = {"limit": 10}
 
-    # Get the Response from Reddit API
     try:
-        res = requests.get(api_uri, headers=user_agent, params=payload, allow_redirects=False)
-        
+        res = requests.get(
+            api_uri, headers=user_agent, params=payload, allow_redirects=False
+        )
+
         # Check if subreddit is invalid
         if res.status_code != 200:
             print("None")
@@ -33,9 +34,9 @@ def top_ten(subreddit):
             return
 
         # Check if the response contains expected data
-        if res_json.get('data') and res_json.get('data').get('children'):
-            for post in res_json.get('data').get('children'):
-                print(post.get('data', {}).get('title', "None"))
+        if res_json.get("data") and res_json.get("data").get("children"):
+            for post in res_json.get("data").get("children"):
+                print(post.get("data", {}).get("title", "None"))
         else:
             print("None")
 
