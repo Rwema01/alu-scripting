05a05a5d022d94c4efa805123189d92398c56692
Rwema01 <r.gisa@alustudent.com>
2025-02-21 21:47:51 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,28 +3,28 @@
 
 import requests
 
-
 def top_ten(subreddit):
     """Return the titles of the top 10 hot posts for a given subreddit.
     
     If subreddit is valid, return the titles. If not, return None.
     """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         json_data = response.json()
         if json_data.get('data') and json_data.get('data').get('children'):
-            for i in range(10):
-                print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-            return  # Successfully printed top 10, exit function
+            for post in json_data.get('data').get('children'):
+                print(post.get('data').get('title'))
         else:
-            print("OK")
+            print(None)
     else:
-        print("OK")
+        print(None)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
