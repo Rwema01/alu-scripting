6eed21e7fab9fb9cd4e885790a9e97def3a23da9
Rwema01 <r.gisa@alustudent.com>
2025-02-21 21:28:42 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,8 +2,16 @@
 """
 1-top_ten: Queries the Reddit API and prints the titles of the first 10 hot posts
 for a given subreddit.
+
+Requirements:
+- Prints the first 10 hot post titles if the subreddit exists.
+- Prints None if the subreddit does not exist.
+- Does not follow redirects.
+- Uses a proper User-Agent to avoid request blocking.
+- Adheres to PEP8 style guide.
 """
 
+import json
 import requests
 
 def top_ten(subreddit):
@@ -18,9 +26,15 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     
     if response.status_code == 200:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
+        try:
+            data = response.json()
+            posts = data.get("data", {}).get("children", [])
+            if not posts:
+                print(None)
+            else:
+                for post in posts:
+                    print(post.get("data", {}).get("title"))
+        except (json.JSONDecodeError, KeyError):
+            print(None)
     else:
         print(None)
